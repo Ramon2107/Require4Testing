8b51e04d3b71e2572410c0bab99e79c1444e5e0f
Ramon2107 <ramon.esteves-salgueiro@iu-study.org>
2025-11-29 22:47:33 +0100

    Create Require4TestingApplication.java

---
+++ src/main/java/com/iu/require4testing/Require4TestingApplication.java
@@ -0,0 +1,20 @@
+package com.iu.require4testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hauptanwendungsklasse für Require4Testing.
+ * Dies ist die Entry-Point-Anwendung für die Web-App zur Organisation manueller Anwendertests.
+ *
+ * @author Require4Testing Team
+ * @version 1.0.0
+ */
+@SpringBootApplication
+public class Require4TestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Require4TestingApplication.class, args);
+	}
+
+}
